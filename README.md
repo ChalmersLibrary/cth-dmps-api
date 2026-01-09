6982ca6ee3a41431f5aa82e988490b58673e8e3a
Urban Andersson <urban.andersson@chalmers.se>
2026-01-09 14:59:43 +0100

    Added GET methods from the Common Machine-Actionable DMP API specification, together with (selected) filters and messages

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # cth-dmps-api
 
-API web service for searching and retrieving DMPs from Chalmers University of Technology in RDA Common maDMP 1.1 format, using Lucene queries. 
+API web service for searching and retrieving DMPs from Chalmers University of Technology in RDA Common maDMP 1.2 format, using the Common Machine-Actionable DMP API specifications (https://rda-dmp-common.github.io/common-madmp-api) or (for now) standard Lucene queries. 
 
-Before getting started, make sure you have installed [Node.js](https://nodejs.org/en/) and [Visual Studio Code](https://code.visualstudio.com/), and of course that you have access to an ES index containing records in maDMP JSON format. Using Elastic version 7- might require some configuration changes.
+Before getting started, make sure you have installed [Node.js](https://nodejs.org/en/) and [Visual Studio Code](https://code.visualstudio.com/), and of course that you have access to an ES index containing records in maDMP JSON format.  
 
 1. Clone this repository.
 2. Run *npm install* from inside the directory.
@@ -14,6 +14,10 @@ Before getting started, make sure you have installed [Node.js](https://nodejs.or
 8. Go to localhost:3000 in your browser or REST client.
 
 A script for creating maDMP JSON records from DS Wizard is avilable at https://github.com/ChalmersLibrary/dsw2es.
+
+## Notes  
+* This app currently only supports the GET method and selected search options and filters. It is also possible to use Lucene queries in the "query" request param.    
+* Using Elasticsearch 7 or over might require some code changes.
 
 ## Documentation
 todo
--- app.js
+++ app.js
@@ -19,6 +19,76 @@ const { equal } = require('assert');
 //   cert: fs.readFileSync(process.env.SSL_CERT || 'cert.pem')
 // };
 
+// Authentication
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    
+    if (token == process.env.JWT_SECRET) {
+        console.log("Authenticated!");
+        next();
+    } else {
+        console.log("NOT Authenticated!");
+        return res.status(401).json({
+            "error_message": "Authentication required",
+            "error_code": "authentication_required"
+        });
+    }
+};
+
+// Check Accept header
+const requireJsonAccept = (req, res, next) => {
+    const accept = req.headers['accept'];
+    if (accept && 
+        !accept.includes('application/json') && 
+        !accept.includes('application/vnd.org.rd-alliance.dmp-common.v1.2+json') && 
+        !accept.includes('*/*')) {
+        return res.status(406).json({
+            "error_message": "Client must accept application/json or application/vnd.org.rd-alliance.dmp-common.v1.2+json",
+            "error_code": "not_acceptable"
+        });
+    }
+    next();
+};
+
+// Validate query params and return 400 on malformed input
+const validateRequest = (req, res, next) => {
+  const count = req.query['count'];
+  const offset = req.query['offset'];
+
+  // if provided, count must be a non-negative integer
+  if (typeof count !== 'undefined') {
+    if (!/^\d+$/.test(String(count)) || parseInt(count, 10) < 0) {
+      return res.status(400).json({
+        "error_message": "Invalid 'count' parameter; must be non-negative integer"
+      });
+    }
+  }
+
+  // if provided, offset must be a non-negative integer
+  if (typeof offset !== 'undefined') {
+    if (!/^\d+$/.test(String(offset)) || parseInt(offset, 10) < 0) {
+      return res.status(400).json({
+        "error_message": "Invalid 'offset' parameter; must be non-negative integer"
+      });
+    }
+  }
+  next();
+};
+
+// Reject methods other than GET (allow OPTIONS and HEAD for CORS/preflight)
+const allowGetOnly = (req, res, next) => {
+  const allowed = ['GET', 'OPTIONS', 'HEAD'];
+  if (!allowed.includes(req.method)) {
+    res.set('Allow', allowed.join(', '));
+    return res.status(405).json({
+      "error_message": "Method not allowed - only GET is supported for now.",
+      "error_code": "method_not_allowed"
+    });
+  }
+  next();
+};
+
 var indexName = process.env.ES_INDEX || 'dsw-dmps';
 
 app.use(bodyParser.json());
@@ -28,7 +98,7 @@ var port = process.env.PORT || 3000;
 // Set path to serve static files.
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Enable CORS.
+// Enable CORS
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET');
@@ -36,43 +106,125 @@ app.use(function (req, res, next) {
   next();
 });
 
+// Enforce allowed methods after CORS headers are set
+app.use(allowGetOnly);
+
 app.get('/', function (req, res) {
     res.sendFile('index.html', {
       root: path.join(__dirname, 'views'),
     });
   });
 
-app.get('/api/v0', function (req, res) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (token == process.env.JWT_SECRET) {
-    console.log("Authenticated!");
-  } 
-  else {
-    console.log("NOT Authenticated!");
-    return res.sendStatus(403);
-  }
+// GET DMP(s) (with params)
+// spec: https://rda-dmp-common.github.io/common-madmp-api/#/DMP/listDMPs
+app.get('/dmps/:dmp_id?', authenticateToken, requireJsonAccept, validateRequest, function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+
+    const dmpId = req.params['dmp_id'];
+    // If a dmp_id is provided in the path, search by _id; otherwise use query param or match-all
+    let queryStr;
+    if (dmpId) {
+      queryStr = 'dmp.dmp_id.identifier"' + dmpId + '"';
+    } else {
+      queryStr = req.query['query'] || '*:*';
+    }
+    
+    // ES params
+    count = req.query['count'] || 9999;
+    offset = req.query['offset'] || 0;
+    sortBy = req.query['sort'] || '_id:asc';
+
+    // Sort params mapping (only implement created and modified for now)
+    if (sortBy == 'created,asc') {
+      sortBy = 'dmp.created:asc';
+    } else if (sortBy == 'created,desc') {
+      sortBy = 'dmp.created:desc';
+    } else if (sortBy == 'modified,asc') {
+      sortBy = 'dmp.modified:asc';
+    } else if (sortBy == 'modified,desc') {
+      sortBy = 'dmp.modified:desc';
+    }
+
+    // Ethical issues param
+    ethical_issues_exists = req.query['ethical_issues_exist'] || null;
+    if (ethical_issues_exists != null) { 
+      if (ethical_issues_exists.toLowerCase() == 'true') {
+        if (queryStr != '*:*') {
+          queryStr = queryStr + ' AND ' + 'dmp.ethical_issues_exists:yes';
+        } else {  
+          queryStr = 'dmp.ethical_issues_exists:yes';
+        }
+      } else if (ethical_issues_exists.toLowerCase() == 'false') {
+        if (queryStr != '*:*') {
+          queryStr = queryStr + ' AND ' + 'dmp.ethical_issues_exists:no';
+        } else {
+          queryStr = 'NOT dmp.ethical_issues_exists:no';
+        }
+      } else {
+        // do nothing   
+      }
+    }
+
+    // todo: implement filtering based on other params. For now ES query is passed directly via 'query' param.
+
+    // Perform the actual search passing in the index, the search query, and the type.
+    client
+      .search({ index: indexName, type: 'dmp', from: offset, size: count, q: queryStr, sort: sortBy })
+      .then((results) => {
+        console.log(results);
+
+        // Get, format and return dmp data
+        var dmpArray = results.body.hits.hits.map(function(hit) {
+          if (req.headers['include-metadata'] == "True") {
+            return hit._source;  
+          } else {
+            var record = JSON.parse(JSON.stringify({ dmp: hit._source.dmp}  ));
+            return record;
+          }
+        });
+
+        var resCount = results.body.hits.total;
+        if (dmpId && resCount == 0) {
+          return res.status(404).json({
+            "error_message": "No DMP found for the provided dmp_id",
+            "error_code": "dmp_not_found"
+          });
+        }   
+        // Create JSON response
+        var r = {};
+        var currDateUtc = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + ' UTC' 
+        r['application'] = 'cth-dmps-api';
+        r['time'] = currDateUtc;
+        if (req.headers['user-agent']) {
+          r['caller'] = req.headers['user-agent'];
+        }
+        r['code'] = res.statusCode;
+        if (res.statusMessage) {
+          r['message'] = res.statusMessage
+        }
+        r['total_count'] = resCount;
+        r['items'] = dmpArray;
+        r['errors'] = []; // todo
+        res.json(r);   
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send([]);
+      });
+  });
+
+app.get('/api/v0', authenticateToken, function (req, res) {
   res.sendFile('api.html', {
     root: path.join(__dirname, 'views'),
   });
 });
 
-app.get('/api/v0/search', function (req, res) {
+app.get('/api/v0/search', authenticateToken, requireJsonAccept, function (req, res) {
     res.setHeader('Content-Type', 'application/json');
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    //const payload = jwt.verify(token, process.env.JWT_SECRET)
-    if (token == process.env.JWT_SECRET) {
-        console.log("Authenticated!");
-    } 
-    else {
-        console.log("NOT Authenticated!");
-        return res.sendStatus(403);
-    }
-
+    
     // Perform the actual search passing in the index, the search query, and the type.
     client
-      .search({ index: indexName, type: 'dmp', from: 0, size: 999, q: req.query['q'],  })
+      .search({ index: indexName, type: 'dmp', from: 0, size: 9999, q: req.query['q'],  })
       .then((results) => {
         console.log(results);
 
@@ -105,7 +257,7 @@ app.get('/api/v0/search', function (req, res) {
             r['message'] = 'OK';
           }   
         }
-        r['total_items'] = resCount;
+        r['total_count'] = resCount;
         r['items'] = dmpArray;
         r['errors'] = []; // todo
         res.json(r);   
@@ -118,19 +270,9 @@ app.get('/api/v0/search', function (req, res) {
   });
   
   // Retrieve plan by (unique) id.
-  app.get('/api/v0/plans/:planId', function (req, res) {
+  app.get('/api/v0/plans/:planId', authenticateToken, requireJsonAccept, function (req, res) {
     res.setHeader('Content-Type', 'application/json');
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    //const payload = jwt.verify(token, process.env.JWT_SECRET)
-    if (token == process.env.JWT_SECRET) {
-        console.log("Authenticated!");
-    } 
-    else {
-        console.log("NOT Authenticated!");
-        return res.sendStatus(403);
-    }
-   
+    
     client
       .search({ index: indexName, type: 'dmp', from: 0, size: 999, q: '_id:"' + req.params['planId'] + '"',  })
       .then((results) => {
@@ -163,7 +305,7 @@ app.get('/api/v0/search', function (req, res) {
             r['message'] = 'OK';
           }   
         }
-        r['total_items'] = resCount;
+        r['total_count'] = resCount;
         r['items'] = dmpArray;
         r['errors'] = []; // todo
         res.json(r);   
